81e07b0bdb997b71f36f5f1492d28ffcdf9ef1cb
colbyfayock <fayock@gmail.com>
2020-10-18 22:03:55 -0400

    header tweaks to prevent mobile spacing issues

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
+import Container from 'components/Container';
+
 import styles from './Header.module.scss';
 
 const Header = ({ children }) => {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>{children}</div>
+      <Container>{children}</Container>
     </header>
   );
 };
