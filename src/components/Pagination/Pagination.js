e592cc66bfa503aa21983930375f9bcd72b7fdeb
Guillermo Angulo <anyermo@gmail.com>
2021-03-08 18:27:14 -0300

    Update pagination component to handle a greater number of pages

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,13 +1,13 @@
-import { useMemo } from 'react';
 import Link from 'next/link';
 
 import config from '../../../package.json';
 import { Helmet } from 'react-helmet';
 
 import { GrPrevious as PreviousIcon, GrNext as NextIcon } from 'react-icons/gr';
+import { HiOutlineDotsHorizontal as Dots } from 'react-icons/hi';
 import styles from './Pagination.module.scss';
 
-const MAX_NUM_LINKS = 10;
+const MAX_NUM_PAGES = 9;
 
 const { homepage = '' } = config;
 
@@ -17,9 +17,33 @@ const Pagination = ({ pagesCount, currentPage, basePath }) => {
   const hasPreviousPage = pagesCount > 1 && currentPage > 1;
   const hasNextPage = pagesCount > 1 && currentPage < pagesCount;
 
-  const pages = useMemo(() => {
-    return [...new Array(pagesCount)].map((_, i) => i + 1);
-  }, [pagesCount]);
+  let hasPrevDots = false;
+  let hasNextDots = false;
+
+  function getPages() {
+    let pages = pagesCount;
+    let start = 0;
+    // If the number of pages exceeds the max
+    if (pagesCount > MAX_NUM_PAGES) {
+      // Set number of pages to the max
+      pages = MAX_NUM_PAGES;
+      const half = Math.ceil(MAX_NUM_PAGES / 2);
+      const isHead = currentPage <= half;
+      const isTail = currentPage > pagesCount - half;
+      hasNextDots = !isTail;
+      // If the current page is at the head, the start variable remains 0
+      if (!isHead) {
+        hasPrevDots = true;
+        // If the current page is at the tail, the start variable is set to
+        // the last chunk. Otherwise the start variable will place the current
+        // page at the middle
+        start = isTail ? pagesCount - MAX_NUM_PAGES : currentPage - half;
+      }
+    }
+    return [...new Array(pages)].map((_, i) => i + 1 + start);
+  }
+
+  const pages = getPages();
 
   return (
     <>
@@ -39,6 +63,11 @@ const Pagination = ({ pagesCount, currentPage, basePath }) => {
         )}
 
         <ul className={styles.pages}>
+          {hasPrevDots && (
+            <li className={styles.dots}>
+              <Dots aria-label={`Navigation to pages 1-${pages[0] - 1} hidden`} />
+            </li>
+          )}
           {pages.map((page) => {
             const active = page === currentPage;
             return active ? (
@@ -57,6 +86,11 @@ const Pagination = ({ pagesCount, currentPage, basePath }) => {
               </li>
             );
           })}
+          {hasNextDots && (
+            <li className={styles.dots}>
+              <Dots aria-label={`Navigation to pages ${pages[pages.length - 1] + 1}-${pagesCount} hidden`} />
+            </li>
+          )}
         </ul>
 
         {hasNextPage && (
