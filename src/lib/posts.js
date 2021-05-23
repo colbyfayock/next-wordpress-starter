d04aadf105c7aeaee476e17022c73d0f110d9de5
colbyfayock <fayock@gmail.com>
2021-05-23 16:52:12 -0400

    fixing excerpt sanitation

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -208,8 +208,19 @@ export function sanitizeExcerpt(excerpt) {
 
   let sanitized = excerpt;
 
-  sanitized = sanitized.replace(/\s?\[&hellip;\]/, '...');
-  sanitized = sanitized.replace('....', '...');
+  // If the theme includes [...] as the more indication, clean it up to just ...
+
+  sanitized = sanitized.replace(/\s?\[&hellip;\]/, '&hellip;');
+
+  // If after the above replacement, the ellipsis includes 4 dots, it's
+  // the end of a setence
+
+  sanitized = sanitized.replace('....', '.');
+  sanitized = sanitized.replace('.&hellip;', '.');
+
+  // If the theme is including a "Continue..." link, remove it
+
+  sanitized = sanitized.replace(/\w*<a class="more-link".*<\/a>/, '');
 
   return sanitized;
 }
