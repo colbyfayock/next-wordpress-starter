37a2cef29f8697e18ab83d70c6aa8e90e7cf0301
Leonardo Losoviz <leo@getpop.org>
2021-08-12 12:05:15 +0800

    Linting code had made it failed

--- src/lib/menus.js
+++ src/lib/menus.js
@@ -93,9 +93,9 @@ export function findMenuByLocation(menus, location) {
   }
 
   do {
-    menu = menus.find(({ locations }) =>
-      locations.map((loc) => loc.toUpperCase()).includes(location.shift().toUpperCase())
-    );
+    menu = menus.find(function ({ locations }) {
+      return locations.map((loc) => loc.toUpperCase()).includes(location.shift().toUpperCase());
+    });
   } while (!menu && location.length > 0);
 
   return parseHierarchicalMenu(menu.menuItems);
