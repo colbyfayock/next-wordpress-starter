bd4e59df1eb78cd42c83b8b33e718ae270111255
Guillermo Angulo <anyermo@gmail.com>
2021-10-24 10:53:07 -0300

    Add no-password param for post queries

--- src/data/posts.js
+++ src/data/posts.js
@@ -102,7 +102,7 @@ export const QUERY_POST_BY_SLUG = gql`
 
 export const QUERY_POSTS_BY_CATEGORY_ID = gql`
   query PostsByCategoryId($categoryId: Int!) {
-    posts(where: { categoryId: $categoryId }) {
+    posts(where: { categoryId: $categoryId, hasPassword: false }) {
       edges {
         node {
           author {
@@ -154,7 +154,7 @@ export const QUERY_POSTS_BY_CATEGORY_ID = gql`
 
 export const QUERY_POSTS_BY_AUTHOR_SLUG = gql`
   query PostByAuthorSlug($slug: String!) {
-    posts(where: { authorName: $slug }) {
+    posts(where: { authorName: $slug, hasPassword: false }) {
       edges {
         node {
           categories {
