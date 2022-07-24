a5e8898c0e661272d1404fc2ca1b494783e66c56
colbyfayock <fayock@gmail.com>
2022-07-24 10:59:31 -0400

    setting dynamic paths to build on demanD

--- src/pages/authors/[slug].js
+++ src/pages/authors/[slug].js
@@ -64,6 +64,6 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   };
 }
--- src/pages/posts/page/[page].js
+++ src/pages/posts/page/[page].js
@@ -43,6 +43,6 @@ export async function getStaticPaths() {
   });
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   };
 }
