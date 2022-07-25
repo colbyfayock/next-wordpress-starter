4006586f2f1ba47d91c6c312a6279f60095b05ed
colbyfayock <fayock@gmail.com>
2022-07-25 10:45:20 -0400

    disabling all plugins to check query

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const withPlugins = require('next-compose-plugins');
 
-const indexSearch = require('./plugins/search-index');
-const feed = require('./plugins/feed');
-const sitemap = require('./plugins/sitemap');
+// const indexSearch = require('./plugins/search-index');
+// const feed = require('./plugins/feed');
+// const sitemap = require('./plugins/sitemap');
 // const socialImages = require('./plugins/socialImages');
 
-module.exports = withPlugins([[indexSearch], [feed], [sitemap]], {
+module.exports = withPlugins([], {
   // By default, Next.js removes the trailing slash. One reason this would be good
   // to include is by default, the `path` property of the router for the homepage
   // is `/` and by using that, would instantly create a redirect
