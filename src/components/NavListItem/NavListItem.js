642836c64828622789d3478a22b840d16531c2ee
rickknowlton <richard.d.knowlton@gmail.com>
2023-07-10 21:12:11 -0400

    fix(NavListItem): add null check for item.path, closes #411

--- src/components/NavListItem/NavListItem.js
+++ src/components/NavListItem/NavListItem.js
@@ -9,12 +9,12 @@ const NavListItem = ({ className, item }) => {
 
   return (
     <li key={item.id}>
-      {!item.path.includes('http') && !item.target && (
+      {item.path && !item.path.includes('http') && !item.target && (
         <Link href={item.path} title={item.title}>
           {item.label}
         </Link>
       )}
-      {item.path.includes('http') && (
+      {item.path && item.path.includes('http') && (
         <a href={item.path} title={item.title} target={item.target}>
           {item.label}
         </a>
